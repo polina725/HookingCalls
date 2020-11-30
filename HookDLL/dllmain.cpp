56eb8ff6add71c5eb3305acc6203dd9352027fbc
polina725 <62598798+polina725@users.noreply.github.com>
2020-11-30 20:43:15 +0300

    Добавьте файлы проекта.

---
+++ HookDLL/dllmain.cpp
@@ -0,0 +1,173 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+#include ".\include\detours.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#pragma comment(lib,"detours.lib")
+using namespace std;
+
+HANDLE hStdOut;
+wofstream outputFile;
+
+HANDLE(WINAPI* pCreateFile) (LPCWSTR, DWORD, DWORD, LPSECURITY_ATTRIBUTES, DWORD, DWORD, HANDLE) = CreateFile;
+BOOL(WINAPI* pReadFile) (HANDLE, LPVOID, DWORD, LPDWORD, LPOVERLAPPED) = ReadFile;
+LSTATUS(WINAPI* pRegSetValue) (HKEY, LPCWSTR, DWORD, LPCWSTR, DWORD) = RegSetValue;
+LONG(WINAPI* pRegOpenKey) (HKEY, LPCWSTR, PHKEY) = RegOpenKey;
+LSTATUS(WINAPI* pRegCloseKey) (HKEY) = RegCloseKey;
+BOOL(WINAPI* pDeleteFile) (LPCWSTR) = DeleteFile;
+LSTATUS(WINAPI* pRegDeleteKey) (HKEY hKey, LPCWSTR lpSubKey) = RegDeleteKey;
+LSTATUS(WINAPI* pRegCreateKey) (HKEY hKey, LPCWSTR lpSubKey, PHKEY phkResult) = RegCreateKey;
+
+VOID displayMessage(wstring);
+VOID writeToLogFile(wstring);
+
+HANDLE WINAPI hookCreateFile(LPCWSTR, DWORD, DWORD, LPSECURITY_ATTRIBUTES, DWORD, DWORD, HANDLE);
+BOOL WINAPI hookReadFile(HANDLE, LPVOID, DWORD, LPDWORD, LPOVERLAPPED);
+BOOL WINAPI hookDeleteFile(LPCWSTR);
+LSTATUS WINAPI hookRegSetValue(HKEY, LPCWSTR, DWORD, LPCWSTR, DWORD);
+LONG WINAPI hookRegOpenKey(HKEY, LPCWSTR, PHKEY);
+LSTATUS WINAPI hookRegCloseKey(HKEY);
+LSTATUS WINAPI hookRegDeleteKey(HKEY, LPCWSTR);
+LSTATUS WINAPI hookRegCreateKey(HKEY, LPCWSTR, PHKEY);
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH: {
+        hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+        TCHAR str[MAX_PATH] = { 0 };
+        GetModuleFileName(hModule, str, MAX_PATH);
+        WCHAR sep = '/';
+
+#ifdef _WIN32
+        sep = '\\';
+#endif
+        std::wstring::size_type pos = std::wstring(str).find_last_of(sep);// L"\\/"
+        wstring path = wstring(str).substr(0, pos);
+        outputFile.open(path + L"\\log_1.txt");
+
+        DisableThreadLibraryCalls(hModule);
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+
+        DetourAttach(&(PVOID&)pCreateFile, hookCreateFile);
+        DetourAttach(&(PVOID&)pReadFile, hookReadFile);
+        DetourAttach(&(PVOID&)pDeleteFile, hookDeleteFile);
+        DetourAttach(&(PVOID&)pRegSetValue, hookRegSetValue);
+        DetourAttach(&(PVOID&)pRegOpenKey, hookRegOpenKey);
+        DetourAttach(&(PVOID&)pRegCloseKey, hookRegCloseKey);
+        DetourAttach(&(PVOID&)pRegCreateKey, hookRegCreateKey);
+
+        DetourTransactionCommit();
+    }
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+
+        DetourDetach(&(PVOID&)pCreateFile, hookCreateFile);
+        DetourDetach(&(PVOID&)pReadFile, hookReadFile);
+        DetourDetach(&(PVOID&)pDeleteFile, hookDeleteFile);
+        DetourDetach(&(PVOID&)pRegSetValue, hookRegSetValue);
+        DetourDetach(&(PVOID&)pRegOpenKey, hookRegOpenKey);
+        DetourDetach(&(PVOID&)pRegCloseKey, hookRegCloseKey);
+        DetourDetach(&(PVOID&)pRegCreateKey, hookRegCreateKey);
+
+        DetourTransactionCommit();
+
+        CloseHandle(hStdOut);
+        break;
+    }
+    return TRUE;
+}
+
+HANDLE WINAPI hookCreateFile(LPCWSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode, LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+    DWORD dwCreationDisposition, DWORD dwFlagsAndAttributes, HANDLE hTemplateFile) {
+    wostringstream message;
+    message << L"CreateFile() was called. File name: " << (wstring)lpFileName << L"\n";
+    displayMessage(message.str());
+    writeToLogFile(message.str());
+    return pCreateFile(lpFileName, dwDesiredAccess, dwShareMode, lpSecurityAttributes, dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile);
+}
+
+BOOL WINAPI hookReadFile(HANDLE hFile, LPVOID lpBuffer, DWORD nNumberOfBytesToRead, LPDWORD lpNumberOfBytesRead, LPOVERLAPPED lpOverlapped) {
+    wostringstream message;
+    message << L"ReadFile() was called. Bytes to read: " << nNumberOfBytesToRead << L"\n";
+    displayMessage(message.str());
+    writeToLogFile(message.str());
+    return pReadFile(hFile, lpBuffer, nNumberOfBytesToRead, lpNumberOfBytesRead, lpOverlapped);
+}
+
+LSTATUS WINAPI hookRegSetValue(HKEY hKey, LPCWSTR lpSubKey, DWORD dwType, LPCWSTR lpData, DWORD cbData) {
+    wostringstream message;
+    message << L"RegSetValue() was called. Subkey: " << (wstring)lpSubKey << L". Data: " << (wstring)lpData << L"\n";
+    displayMessage(message.str());
+    writeToLogFile(message.str());
+    return pRegSetValue(hKey, lpSubKey, dwType, lpData, cbData);
+}
+
+LONG WINAPI hookRegOpenKey(HKEY hKey, LPCWSTR lpSubKey, PHKEY phkResult) {
+    wostringstream message;
+    message << L"RegOpenKey() was called. Subkey: " << (wstring)lpSubKey << L"\n";
+    displayMessage(message.str());
+    writeToLogFile(message.str());
+    return pRegOpenKey(hKey, lpSubKey, phkResult);
+}
+
+LSTATUS WINAPI hookRegCloseKey(HKEY hKey) {
+    wostringstream message;
+    message << L"RegCloseKey() was called.\n";
+    displayMessage(message.str());
+    writeToLogFile(message.str());
+    return pRegCloseKey(hKey);
+}
+
+BOOL WINAPI hookDeleteFile(LPCWSTR lpFileName)
+{
+    wostringstream message;
+    message << L"DeleteFile() was called. File name: " << (wstring)lpFileName << L"\n";
+    displayMessage(message.str());
+    writeToLogFile(message.str());
+    return pDeleteFile(lpFileName);
+}
+
+LSTATUS WINAPI hookRegDeleteKey(HKEY hKey, LPCWSTR lpSubKey)
+{
+    wostringstream message;
+    message << L"RegDeleteKey() was called. Subkey: " << (wstring)lpSubKey << L"\n";
+    displayMessage(message.str());
+    writeToLogFile(message.str());
+    return pRegDeleteKey(hKey, lpSubKey);
+}
+
+LSTATUS WINAPI hookRegCreateKey(HKEY hKey, LPCWSTR lpSubKey, PHKEY phkResult)
+{
+    wostringstream message;
+    message << L"RegCreateKey() was called. Subkey: " << (wstring)lpSubKey << L"\n";
+    displayMessage(message.str());
+    writeToLogFile(message.str());
+    return pRegCreateKey(hKey, lpSubKey, phkResult);
+}
+
+VOID displayMessage(wstring message)
+{
+    if (hStdOut != NULL) {
+        WriteConsole(hStdOut, message.c_str(), message.size(), NULL, NULL);
+
+    }
+}
+
+VOID writeToLogFile(wstring message) {
+    if (outputFile.is_open()) {
+        outputFile << message << endl;
+        outputFile.flush();
+    }
+}
---
+++ HookingCalls/HookingCalls.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    LPCWSTR appName = L".\\AppToHook.exe";
+    char injectDLLName[] = ".\\HookDLL.dll";
+    STARTUPINFO si = { sizeof(STARTUPINFO) };
+    PROCESS_INFORMATION pi;
+
+    wstring CommandLine(L"AppToHook.exe");
+
+    LPWSTR lpwCmdLine = &CommandLine[0];
+
+    ZeroMemory(&pi, sizeof(pi));
+
+    si.cb = sizeof(si);
+        
+    if (!CreateProcess(NULL, lpwCmdLine, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi)) {
+        cout << "CreateProcess() failed. Error: " << GetLastError() << endl;
+        return -1;
+    }
+    
+
+    HANDLE process = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, pi.dwProcessId);
+    if (process == NULL)
+    {
+        cout << "OpenProcess() failed. Error: " << GetLastError() << endl;
+        return 1;
+    }
+
+    HMODULE hKernelModule = GetModuleHandle(TEXT("kernel32.dll"));
+    if (hKernelModule == NULL)
+    {
+        cout << "GetModuleHandle() failed. Error: " << GetLastError() << endl;
+        return 1;
+    }
+
+    FARPROC LoadLibraryAddress = GetProcAddress(hKernelModule, "LoadLibraryA");
+    if (LoadLibraryAddress == NULL)
+    {
+        cout << "GetProcAddress() failed. Error: " << GetLastError() << endl;
+        return 1;
+    }
+
+    PVOID rmMemory = VirtualAllocEx(process, NULL, strlen(injectDLLName) + 1, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+    if (rmMemory == NULL)
+    {
+        cout << "VirtualAllocEx() failed. Error: " << GetLastError() << endl;
+        return 1;
+    }
+
+    WriteProcessMemory(process, (LPVOID)rmMemory, injectDLLName, strlen(injectDLLName) + 1, NULL);
+    HANDLE rmThread = CreateRemoteThread(process, NULL, NULL, (LPTHREAD_START_ROUTINE)LoadLibraryAddress, (LPVOID)rmMemory, NULL, NULL);
+    if (rmThread == NULL)
+    {
+        cout << "CreateRemoteThread() failed. Error: " << GetLastError() << endl;
+        return 1;
+    }
+    else {
+        cout << "Injection complete" << endl;
+    }
+
+    ResumeThread(pi.hThread);
+
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+    return 0;
+}
